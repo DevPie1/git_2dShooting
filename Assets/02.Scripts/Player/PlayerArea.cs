ef31f6e9639d486db67e6ddf121d923e8909f025
DevPie1 <andy3201@naver.com>
2026-09-02 12:42:24 +0900

    추가 : 속도 업다운, 영역 제어

---
+++ Assets/02.Scripts/Player/PlayerArea.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerArea : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -5,27 +5,73 @@ public class PlayerMove : MonoBehaviour
     // 목적 : 키보드 입력에 따라서 플레이어 이동 처리를 하고 싶다.
 
     public float Speed = 0.05f;
-    
-    
+    // e키 , 업다운 량, 
+    public RectTransform restrictArea;
     private void Start()
     {
         
     }
 
+    private void SpeedUp()
+    {
+        Speed += 0.03f;
+    }
+    private void SpeedDown()
+    {
+        Speed -= 0.03f;
+    }
+
     private void Update()
-    {   
+    {
         //Vector2 direction = new Vector2(-1, 0);
         //1. 키보드 입력을 받는다.
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        
-        Vector2 direction = new Vector2(h, v);
-        
-        Vector2 normalizedSpeed = (direction * Speed).normalized;
-            //Debug.Log("왼쪽 방향키를 누르는 중");
-            //매직 넘버란? : 보는 사람에 따라 의미가 달라질 수 있는 숫자 
-            transform.Translate(direction * normalizedSpeed*Time.deltaTime);
-        
-        
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            SpeedUp();
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            SpeedDown();
+        }
+
+        Vector2 direction = new Vector2(h, v).normalized;
+        //Debug.Log("왼쪽 방향키를 누르는 중");
+        //매직 넘버란? : 보는 사람에 따라 의미가 달라질 수 있는 숫자 
+        transform.Translate(direction * Speed * Time.deltaTime);
+
+        if (restrictArea != null)
+        {
+            Vector3 currentPos = transform.position;
+            // UI 이미지의 네 모서리 월드 좌표를 가져옵니다.
+            Vector3[] corners = new Vector3[4];
+            restrictArea.GetWorldCorners(corners);
+
+         
+            float minX = corners[0].x;
+            float maxX = corners[2].x;
+            float minY = corners[0].y;
+            float maxY = corners[2].y;
+            
+            if (currentPos.x > maxX)
+            {
+                currentPos.x = minX;
+            }
+            // 세모가 왼쪽 끝(minX)보다 더 나가면 오른쪽 끝(maxX)으로 이동
+            else if (currentPos.x < minX)
+            {
+                currentPos.x = maxX;
+            }
+            
+         
+            float clampedY = Mathf.Clamp(transform.position.y, minY, maxY );
+
+           
+            currentPos.y = Mathf.Clamp(currentPos.y, minY , maxY );
+            transform.position = currentPos;
+        }
     }
 }
