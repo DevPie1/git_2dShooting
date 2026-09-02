5bee3cfde29acee5437885b44aa8fb41a0d15793
DevPie1 <andy3201@naver.com>
2026-09-02 11:11:34 +0900

    Fix : 플레이어 대각선 이동 정규화

--- Assets/02.Scripts/Player/PlayerMove.cs
+++ Assets/02.Scripts/Player/PlayerMove.cs
@@ -16,17 +16,16 @@ public class PlayerMove : MonoBehaviour
     {   
         //Vector2 direction = new Vector2(-1, 0);
         //1. 키보드 입력을 받는다.
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
         
         Vector2 direction = new Vector2(h, v);
+        
+        Vector2 normalizedSpeed = (direction * Speed).normalized;
             //Debug.Log("왼쪽 방향키를 누르는 중");
             //매직 넘버란? : 보는 사람에 따라 의미가 달라질 수 있는 숫자 
-            transform.Translate(direction * Speed*Time.deltaTime);
+            transform.Translate(direction * normalizedSpeed*Time.deltaTime);
         
-        //2. 키보드 입력에 따라 방향을 구한다.
-
         
-        //3. 방향과 속도에 따라 이동한다.
     }
 }
